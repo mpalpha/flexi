51f1f43f96df970649676e50a2cc840e545692ae
Jason Lusk <jason@jasonlusk.com>
2018-05-03 18:13:03 -0500

    update css url

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
-## Flexi
-A grid system based on the flex display property. 
+# Flexi
 
-### [Documentation](https://mpalpha.github.io/flexi/)
+A responsive grid system based on the flex display property.
+
+## Supported Browsers
+
+| ![Chrome](https://raw.githubusercontent.com/mpalpha/browser-logos/master/src/chrome/chrome_32x32.png) | ![Firefox](https://raw.githubusercontent.com/mpalpha/browser-logos/master/src/firefox/firefox_32x32.png) | ![IE](https://raw.githubusercontent.com/mpalpha/browser-logos/master/src/edge/edge_32x32.png) | ![Opera](https://raw.githubusercontent.com/mpalpha/browser-logos/master/src/opera/opera_32x32.png) | ![Safari](https://raw.githubusercontent.com/mpalpha/browser-logos/master/src/safari/safari_32x32.png) |
+| --- | --- | --- | --- | --- |
+| Latest ✔ | Latest ✔ | 11+ ✔ | Latest ✔ | 6.1+ ✔ |
+
+## see [Documentation](https://mpalpha.github.io/flexi/)
+
+## Get Started
 
 To run the documentation locally on your machine, you need [Node.js](https://nodejs.org/en/) installed on your computer. (Your Node.js version must be **6.4.0** or higher). Run these commands to set up the documentation:
 
+### Install
+
 ```bash
-# Install
 git clone https://github.com/mpalpha/flexi.git
 cd flexi
 npm install or yarn install
+```
+
+### Run
+
+```bash
+gulp serve
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,87 @@
-let gulp = require('gulp');
-let sassdoc = require('sassdoc');
-let sass = require('gulp-sass');
-let cleanCSS = require('gulp-clean-css');
-let sourcemaps = require('gulp-sourcemaps');
-let rename = require('gulp-rename');
+const gulp = require('gulp');
+const sassDoc = require('sassdoc');
+const sass = require('gulp-sass');
+const autoprefixer = require('autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const sassLint = require('gulp-sass-lint');
+const sequence = require('run-sequence');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
 
-gulp.task('styles', () => {
-    return gulp.src('scss/**/*.scss')
-        .pipe(sassdoc())
-        .pipe(sourcemaps.init())   
-        .pipe(sass())
-        .pipe(gulp.dest('dist'))
-        .pipe(cleanCSS())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist'));
+// Compiles Sass files into CSS
+gulp.task('styles', ['sass-lint', 'sass:docs'], () => {
+  return sequence('sass');
+});
+
+gulp.task('sass', cb => {
+  gulp
+    .src('scss/**/*.scss')
+    .pipe(sassDoc())
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass().on('error', err => {
+        console.error('\x07'); // output error
+        sass.logError.bind(this)(err);
+      })
+    )
+    .pipe(
+      postcss([
+        autoprefixer({
+          browsers: ['last 2 versions', 'ie >= 9', 'android >= 4.4', 'ios >= 7']
+        })
+      ])
+    )
+    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('docs'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cleanCSS({ compatibility: 'ie9' }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('docs')) && cb();
+});
+
+gulp.task('sass-lint', () => {
+  return gulp
+    .src(['scss/**/*.scss'])
+    .pipe(
+      sassLint({
+        config: './.sass-lint.yml'
+      })
+    )
+    .pipe(sassLint.format());
+});
+
+gulp.task('sass:docs', () => {
+  return gulp
+    .src('docs/demo.scss')
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass().on('error', err => {
+        console.error('\x07'); // output error
+        sass.logError.bind(this)(err);
+      })
+    )
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('docs'));
+});
+
+gulp.task('browserSync', cb => {
+  browserSync.init({
+    server: './docs',
+    files: ['*.css', '*.html'],
+    open: true
+  }) && cb();
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['browserSync'], () => {
+  sequence('sass-lint', 'styles');
+  gulp.watch('**/*.scss', ['styles']);
+  gulp
+    .watch(['docs/*.css', 'docs/*.html'], browserSync.reload)
+    .on('change add', browserSync.reload);
 });
 
 gulp.task('default', ['styles']);
